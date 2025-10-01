5cb46b453546810b0a6c458e9837185bc410e20d
hunseo24 <hunseo0204@gmail.com>
2025-10-01 09:22:12 +0000

    단순 색 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("🎈 나의 새로운 앱")
-st.info("2025년 10월 6일은 추석입니다")
+st.warning("2025년 10월 6일은 추석입니다")
 st.success("즐거운 추석 되세요!")
-st.warning("")
 st.image("https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13297230&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzI5NzIzMF9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004")
