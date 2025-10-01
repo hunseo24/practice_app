4e038c80a5e92f799fb34ab0ca7035e23fec4834
hunseo24 <hunseo0204@gmail.com>
2025-10-01 09:10:48 +0000

    추석 맞이 메인화면 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 나의 새로운 앱")
+st.info("2025년 10월 6일은 추석입니다")
+st.success("즐거운 추석 되세요!")
+st.warning("")
+st.image("https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13297230&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzI5NzIzMF9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004")
